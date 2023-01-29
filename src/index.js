82278b253ed5f6a6eaeb05879e6e884c68c1b693
Carolina Paredes Arias <carolina.paredes.arias@gmail.com>
2023-01-29 15:04:06 -0300

    Comienzo parte B

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import validator from './validator.js';
-
-console.log(validator);
+if (document.getElementById("cardnumber").value.length == 16) {
+  valid = isValid(document.getElementById("cardnumber").value);
+} else {
+  alert("Ingrese los 16 dígitos de su tarjeta");
+}
+if (valid) {
+  document.getElementById('result').innerHTML = "succes";
+} else {
+  document.getElementById('result').innerHTML = "error";
+}
+console.log(valid);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,7 @@
 const validator = {
-  // ...
+  let arr = creditCardNumber.split('').reverse();
+  let impar = 0;
+
 };
 
 export default validator;
