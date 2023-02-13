4aa34ee54297b51c648dd836b3ee02cd2856e8f9
Carolina Paredes Arias <carolina.paredes.arias@gmail.com>
2023-02-13 15:51:43 -0300

    final proyecto 1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 const btnvalid = document.getElementById("btnvalid");
 let valid = false;
 let mask = "";
-
-function validar(){
+function validar() {
   if (document.getElementById("cardnumber").value.length === 16) {
     valid = validator.isValid(document.getElementById("cardnumber").value);
-    //console.log(valid);
   } else {
     alert("Ingrese los 16 dígitos de su tarjeta");
   }
   if (valid) {
     mask = validator.maskify(document.getElementById("cardnumber").value);
     document.getElementById("cardnumber").value = mask;
-    console.log(mask);
-    document.getElementById('result').innerHTML = "succes";
+    document.getElementById("result").innerHTML = "succes";
   } else {
-    document.getElementById('result').innerHTML = "error";
-    //console.log("error");
+    document.getElementById("result").innerHTML = "error";
   }
-} 
+}
 btnvalid.addEventListener("click", validar);
--- src/validator.js
+++ src/validator.js
@@ -1,11 +1,18 @@
 const validator = {
-  
-
   isValid(creditCardNumber) {
-    const arr = creditCardNumber.split('').reverse();
+    const arr = creditCardNumber.split("").reverse();
     let impar = 0;
-    for (let i = 1, l = arr.length; i < l; i++) {
-      if (i === 1 || i === 3 || i === 5 || i === 7 || i === 9 || i === 11 || i === 13 || i === 15) {
+    for (let i = 1; i < arr.length; i++) {
+      if (
+        i === 1 ||
+        i === 3 ||
+        i === 5 ||
+        i === 7 ||
+        i === 9 ||
+        i === 11 ||
+        i === 13 ||
+        i === 15
+      ) {
         const mult = arr[i] * 2;
         if (mult >= 10) {
           const arrayOfDigits = Array.from(String(mult));
@@ -17,33 +24,24 @@ const validator = {
         }
       } else {
         impar = impar + parseInt(arr[i]);
-        //console.log(arr);
       }
     }
-    if (impar % 2 === 0) {
+    if (impar % 10 === 0) {
       return true;
     } else {
       return false;
     }
-
   },
-
   maskify(creditCardNumber) {
-    //guardamos aqui la cadena modificada
     let resultado = "";
-    //recorremos toda la cadena original
     for (let i = 0; i < creditCardNumber.length; i++) {
-      //todos los caracteres seran cambiados a # excepto los 4 ultimos
       if (i < creditCardNumber.length - 4) {
-        resultado = resultado + '#' //consideramos # antes de llegar a los cuatro ultimos
+        resultado = resultado + "#";
       } else {
-        resultado = resultado + creditCardNumber[i] //consideramos los 4 caracteres finales iguales
-        //console.log(this.maskify);
+        resultado = resultado + creditCardNumber[i];
       }
     }
-    return resultado
+    return resultado;
   },
 };
-
-
 export default validator;
